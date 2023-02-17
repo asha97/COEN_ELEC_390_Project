fb427167ec31005b0f32918671bf18152faa0929
Asha I <85359230+asha97@users.noreply.github.com>
2023-02-16 22:52:32 -0500

    adding login and sign buttons for UI + backend #4 #8 #15

--- app/src/main/java/com/example/coen_elec_390_project/MainActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project/MainActivity.java
@@ -3,13 +3,43 @@ package com.example.coen_elec_390_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    //changes
+    Button signUpBtn, loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpBtn = findViewById(R.id.signUpButtonMain);
+        loginBtn = findViewById(R.id.loginButtonHome);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v){
+                goToSignUp();
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void onClick(View v){
+                goToLogin();
+            }
+        });
+    }
+
+    private void goToLogin(){
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
+    }
+
+    private void goToSignUp(){
+        Intent intent = new Intent(this, SignUpPage.class);
+        startActivity(intent);
     }
 }
