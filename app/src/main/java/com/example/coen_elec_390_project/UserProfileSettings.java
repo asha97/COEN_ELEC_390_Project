2af2148bea8b7979348d278a55bf6b4ad76b7c8d
Asha I <85359230+asha97@users.noreply.github.com>
2023-02-19 20:01:44 -0500

    Adding user profile settings and connection settings layout issue #15

---
+++ app/src/main/java/com/example/coen_elec_390_project/UserProfileSettings.java
@@ -0,0 +1,14 @@
+package com.example.coen_elec_390_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileSettings extends AppCompatActivity {
+    //this class is going to be used in order to update the information about the user
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_settings);
+    }
+}
