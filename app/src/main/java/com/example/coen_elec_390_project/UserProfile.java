c933e2cccf7d45a091a108c7900303fdae25994a
Asha I <85359230+asha97@users.noreply.github.com>
2023-03-19 02:08:31 -0400

    changed user profile #60

--- app/src/main/java/com/example/coen_elec_390_project/UserProfile.java
+++ app/src/main/java/com/example/coen_elec_390_project/UserProfile.java
@@ -1,32 +1,69 @@
 package com.example.coen_elec_390_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 public class UserProfile extends AppCompatActivity {
 
-    Button healthBtn;
+    private TextView nameText;
+    private TextView dateText;
+    private TextView locationText;
+    private DatabaseReference userRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        healthBtn = findViewById(R.id.HealthInfoBtn);
+        nameText = findViewById(R.id.nameText);
+        dateText = findViewById(R.id.dateText);
+        locationText = findViewById(R.id.locationText);
 
-        healthBtn.setOnClickListener(new View.OnClickListener() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String uid = currentUser.getUid();
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+        userRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                HealthInfo();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String name = snapshot.child("name").getValue().toString();
+                    String dateOfBirth = snapshot.child("dob").getValue().toString();
+                    String location = snapshot.child("location").getValue().toString();
+
+                    nameText.setText(name);
+                    dateText.setText(dateOfBirth);
+                    locationText.setText(location);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("UserProfile", "loadUser:onCancelled", error.toException());
             }
         });
-    }
 
-    public void HealthInfo() {
-        Intent intent = new Intent(getApplicationContext(), HealthInfo.class);
-        startActivity(intent);
+        Button changeProfileInfo = findViewById(R.id.changeProfileInfo);
+        changeProfileInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the EditProfile activity to allow the user to change their profile information
+                Intent intent = new Intent(UserProfile.this, UserProfileSettings.class);
+                startActivity(intent);
+            }
+        });
     }
 }
